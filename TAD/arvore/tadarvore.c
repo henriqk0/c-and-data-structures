e8e298f73b4ada51f5d295f57c6e2f5b4ea25a6d
Henrique Lima <171154572+henriqk0@users.noreply.github.com>
2025-04-01 10:59:33 -0300

    update tadarvore.c to add integers

--- TAD/arvore/tadarvore.c
+++ TAD/arvore/tadarvore.c
@@ -11,51 +11,54 @@ void inord_print(Pilha x) {
 }
 */
 
-void print_inord_rec(PONT raiz) {
+void print_inord_rec_int(PONT raiz) {
   if (raiz != NULL) {
-    print_inord_rec(raiz->esq);
-    printf("%ld\t", (long int)raiz->chave);
-    print_inord_rec(raiz->dir);
+    print_inord_rec_int(raiz->esq);
+    printf("%d\t", *(int *)raiz->chave);
+    print_inord_rec_int(raiz->dir);
   }
 }
 
-void seetree(PONT raiz) {
+void seetree_int(PONT raiz) {
   if (raiz != NULL) {
-    printf("%ld", (long int)raiz->chave);
+    printf("%d", *(int *)raiz->chave);
     printf("(");
-    seetree(raiz->esq);
-    seetree(raiz->dir);
+    seetree_int(raiz->esq);
+    seetree_int(raiz->dir);
     printf(")");
   }
 }
 
 PONT createNewnNo(t_dado ch) {
-  PONT newno = (PONT)malloc((sizeof(NO)));
+  PONT newno = (PONT)malloc(sizeof(NO));
   newno->esq = NULL;
   newno->dir = NULL;
   newno->chave = ch;
   return newno;
 }
 
-PONT addno(PONT raiz, PONT no) {
+PONT addno(PONT raiz, PONT no, int (*compar)(t_dado, t_dado)) {
   if (raiz == NULL)
-    return (no);
-  if (no->chave < raiz->chave) {
-    raiz->esq = addno(raiz->esq, no);
+    return no;
+
+  if (compar(no->chave, raiz->chave) < 0) {
+    raiz->esq = addno(raiz->esq, no, compar);
   } else {
-    raiz->dir = addno(raiz->dir, no);
-  } // rem. == keys
-  return (raiz);
+    raiz->dir = addno(raiz->dir, no, compar);
+  }
+  return raiz;
 }
 
-PONT contem(t_dado ch, PONT raiz) {
+PONT contem(t_dado ch, PONT raiz, int (*compar)(t_dado, t_dado)) {
   if (raiz == NULL)
     return NULL;
-  if (raiz->chave == ch)
+
+  if (compar(ch, raiz->chave) == 0)
     return raiz;
-  if (raiz->chave > ch)
-    return contem(ch, raiz->esq);
-  return (contem(ch, raiz->dir));
+  else if (compar(ch, raiz->chave) < 0)
+    return contem(ch, raiz->esq, compar);
+  else
+    return contem(ch, raiz->dir, compar);
 }
 
 int numNo(PONT raiz) {
@@ -64,16 +67,28 @@ int numNo(PONT raiz) {
   return (numNo(raiz->esq) + 1 + numNo(raiz->dir));
 }
 
+int compar_int(t_dado a, t_dado b) {
+  return (*(int*)a - *(int*)b);
+}
+
 PONT initialz() { return NULL; }
 
 int main() {
   PONT r = initialz();
-  addno(r, createNewnNo((t_dado)23));
 
-  addno(r, createNewnNo((t_dado)15));
-  addno(r, createNewnNo((t_dado)12));
-  addno(r, createNewnNo((t_dado)25));
-  addno(r, createNewnNo((t_dado)24));
-  addno(r, createNewnNo((t_dado)26));
+  int val1 = 23, val2 = 15, val3 = 12, val4 = 25, val5 = 24, val6 = 26;
+
+  r = addno(r, createNewnNo(&val1), compar_int);
+  r = addno(r, createNewnNo(&val2), compar_int);
+  r = addno(r, createNewnNo(&val3), compar_int);
+  r = addno(r, createNewnNo(&val4), compar_int);
+  r = addno(r, createNewnNo(&val5), compar_int);
+  r = addno(r, createNewnNo(&val6), compar_int);
+
+  print_inord_rec_int(r);
+  printf("\n");
+  seetree_int(r);
+  printf("\n");
+
   return 0;
 }
